89500d6a6d06a6ad8f60dc2bc2de32fdb4a69ec3
Ryuu <thiago_dragon_@hotmail.com>
2017-12-24 01:22:04 -0200

    Applying Anti-Aliasing on resized images
    When an image is being resized, it's now being applied Anti-Aliasing.
    The quality of the images converted to JPEG was reduced.
    Some code was abstracted to functions in order to be more clear and docstrings were rewritten in order to be more descriptive.

--- booru/utils.py
+++ booru/utils.py
@@ -5,7 +5,7 @@ from django.core.files.base import ContentFile
 
 sample_max_resolution = (850, None)
 preview_max_resolution = (150, 150)
-jpg_quality = 90
+jpg_quality = 80
 
 def space_splitter(tag_string):
     return [t.strip().lower() for t in tag_string.split(' ') if t.strip()]
@@ -16,31 +16,28 @@ def space_joiner(tags):
 def get_sample(original_pil_image):
     ''' Returns the resized sample image. '''
     pil_image = reduce_image_to_maximum_size(original_pil_image, sample_max_resolution)
+    pil_image = convert_to_rgb(pil_image)
+    pil_image = convert_image_to_jpeg_bytes(pil_image)
 
     if pil_image:
-        return get_converted_image(pil_image)
+        return convert_bytes_to_content_file(pil_image)
     else:
         return None
 
 def get_preview(original_pil_image):
     ''' Returns the resized preview image. '''
     pil_image = reduce_image_to_maximum_size(original_pil_image, preview_max_resolution)
+    pil_image = convert_to_rgb(pil_image)
+    pil_image = convert_image_to_jpeg_bytes(pil_image)
 
     if pil_image:
-        return get_converted_image(pil_image)
+        return convert_bytes_to_content_file(pil_image)
     else:
-        return get_converted_image(original_pil_image)
-
-def get_converted_image(pil_image):
-    ''' Convert the PIL Image to ImageContent in RGB mode and returns it. '''
-    # Converting to RBG if RGBA
-    if pil_image.mode == 'RGBA':
-        pil_image = convert_to_rgb(pil_image)
-
-    return convert_to_content_file(pil_image, 'JPEG')
+        return convert_bytes_to_content_file(original_pil_image)
 
 def reduce_image_to_maximum_size(image, max_resolution):
-    ''' Reduce image to be equal to the maximum resolution allowed (if less, it returns None). If one of the max_resolution's components is equal to None, it will be ignored.'''
+    ''' Reduce the given image to be equal to the maximum resolution allowed (or None if already lower).
+    If one of the max_resolution's components is equal to None, it will be ignored.'''
     original_size = image.size
 
     max_resolution = list(max_resolution)
@@ -64,33 +61,45 @@ def reduce_image_to_maximum_size(image, max_resolution):
     else:
         new_size = (int(round(original_size[0] / proportion)), int(round(original_size[1] / proportion)))
 
-        return image.resize(new_size)
+        return image.resize(new_size, ImagePIL.ANTIALIAS)
 
-def convert_to_content_file(pil_image, img_format):
-    ''' Returns a ContentFile from a PIL Image. '''    
+def convert_image_to_jpeg_bytes(pil_image):
+    ''' Convert an PIL Image to JPEG and returns it as bytes. '''
     if pil_image == None:
         return None
 
     f = BytesIO()
-    pil_image.save(f, img_format, quality=jpg_quality)
+    pil_image.save(f, format='JPEG', quality=jpg_quality)
 
-    s = f.getvalue()
-    f.close()
+    return f
+
+def convert_bytes_to_content_file(pil_image_bytes):
+    ''' Convert the bytes of an Image to a Django ContentFile. '''
+    if pil_image_bytes == None:
+        return None
+
+    s = pil_image_bytes.getvalue()
+    pil_image_bytes.close()
 
     return ContentFile(s)
 
 def get_pil_image_if_valid(image):
-    ''' Returns a PIL Image if the image is valid, return False otherwise. '''
+    ''' Returns a PIL Image if the given image file is valid, returns False otherwise. '''
     try:
         return ImagePIL.open(image)
     except:
         return False
 
 def convert_to_rgb(pil_image):
-    ''' Converts a RGBA image to RGB. '''
-    pil_image.load()
+    ''' Converts an image from RGBA to RGB if needed. '''
 
-    rgb_image = ImagePIL.new("RGB", pil_image.size, (255, 255, 255))
-    rgb_image.paste(pil_image, mask=pil_image.split()[3]) # 3 is the alpha channel
+    # Converting to RBG if RGBA
+    if pil_image.mode == 'RGBA':
+        pil_image.load()
 
-    return rgb_image
+        rgb_image = ImagePIL.new("RGB", pil_image.size, (255, 255, 255))
+        rgb_image.paste(pil_image, mask=pil_image.split()[3]) # 3 is the alpha channel
+
+        return rgb_image
+    else:
+        return pil_image
