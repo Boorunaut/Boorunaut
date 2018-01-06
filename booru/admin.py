bbd26874defb831c08bd24f987d7f598461d2657
Luk3M <luk3@hotmail.com.br>
2018-01-06 02:29:52 -0200

    Creates a new Tag substitute called "PostTag", and links TaggedPost to it. Also links the categories to it instead, that was the expected behaviour.

--- booru/admin.py
+++ booru/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post, Category, TaggedPost
+from .models import Post, Category, PostTag
 
 admin.site.register(Post)
 admin.site.register(Category)
-admin.site.register(TaggedPost)
+admin.site.register(PostTag)
