85b96036d2b003166737e9abd2c649f0b36cc270
Ryuu <thiago_dragon_@hotmail.com>
2018-03-05 10:41:34 -0300

    Implemented the Login and Login systems and started the Register system

--- account/models.py
+++ account/models.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class Account(AbstractUser):
     pass
+    '''avatar          = models.ImageField(blank=True, upload_to='avatars/', default='avatars/avatar_default.svg')
+    slug            = models.SlugField(max_length=250, default="", blank=True)
+    email_activated = models.BooleanField(default=False)
+    comments_locked = models.BooleanField(default=False)'''
