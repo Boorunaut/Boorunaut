607d54e36853eada489ee162d0c19a3f36696184
Ryuu <thiago_dragon_@hotmail.com>
2018-03-05 10:46:51 -0300

    Modified the Register system

--- account/views.py
+++ account/views.py
@@ -56,4 +56,38 @@ class RegisterView(FormView):
     """
     Provides the ability to a visitor to register as new user with a username and password
     """
-    pass
+    success_url = '/post/list'
+    form_class = UserCreationForm
+    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = "account/register.html"
+
+    @method_decorator(sensitive_post_parameters('password'))
+    @method_decorator(csrf_protect)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        # Sets a test cookie to make sure the user has cookies enabled
+        request.session.set_test_cookie()
+
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.save()
+
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+
+        user = authenticate(username=username, password=raw_password)
+        auth_login(self.request, user)
+
+        # If the test cookie worked, go ahead and
+        # delete it since its no longer needed
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+
+        return super(RegisterView, self).form_valid(form)
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get(self.redirect_field_name)
+        if not is_safe_url(url=redirect_to, host=self.request.get_host()):
+            redirect_to = self.success_url
+        return redirect_to
