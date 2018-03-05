0f5082e98325b1dcdb9c762a0fb8fe77548148b0
Ryuu <thiago_dragon_@hotmail.com>
2018-03-05 13:53:19 -0300

    Added new fields to the Account model

--- account/models.py
+++ account/models.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Account(AbstractUser):
-    pass
-    '''avatar          = models.ImageField(blank=True, upload_to='avatars/', default='avatars/avatar_default.svg')
+    avatar          = models.ForeignKey('booru.Post', null=True, on_delete=models.SET_NULL)
     slug            = models.SlugField(max_length=250, default="", blank=True)
     email_activated = models.BooleanField(default=False)
-    comments_locked = models.BooleanField(default=False)'''
+    comments_locked = models.BooleanField(default=False)
