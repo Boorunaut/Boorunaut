a3e4cf97405c276a45bfcc861e97542afb6647cd
Luk3M <luk3@hotmail.com.br>
2018-04-26 16:04:05 -0300

    Changed casing of tags on tests to reflect slugs created on taggit.

--- booru/tests/tests_booru_search.py
+++ booru/tests/tests_booru_search.py
@@ -16,11 +16,11 @@ class SearchTests(TestCase):
         test_post2 = Post.objects.create()
         test_post3 = Post.objects.create()
 
-        PostTag.objects.create(name="Test1", slug="test1")
-        PostTag.objects.create(name="Test3", slug="test3")
-        PostTag.objects.create(name="Test4", slug="test4")
-        test2 = PostTag.objects.create(name="Test2", slug="test2")
-        test_two = PostTag.objects.create(name="TestTwo", slug="test_two")
+        PostTag.objects.create(name="test1", slug="test1")
+        PostTag.objects.create(name="test3", slug="test3")
+        PostTag.objects.create(name="test4", slug="test4")
+        test2 = PostTag.objects.create(name="test2", slug="test2")
+        test_two = PostTag.objects.create(name="testtwo", slug="testtwo")
         
         Alias.objects.create(from_tag=test2, to_tag=test_two)
 
@@ -43,7 +43,7 @@ class SearchTests(TestCase):
         tag_string = "test1 test2"
         generated_tags = verify_and_substitute_alias(tag_string)
 
-        expected_generated_tags = ["test1", "test_two"]
+        expected_generated_tags = ["test1", "testtwo"]
         self.assertEqual(generated_tags, expected_generated_tags)
 
     def test_get_single_post_from_tag_list(self):
@@ -70,7 +70,7 @@ class SearchTests(TestCase):
         self.assertEqual(list(generated_tags_post), list(expected_post))
 
     def test_get_no_posts_from_inexistent_tag_list(self):
-        tag_string = "test1 test_two non_existent"
+        tag_string = "test1 testtwo non_existent"
 
         generated_tags = verify_and_substitute_alias(tag_string)
 
@@ -81,7 +81,7 @@ class SearchTests(TestCase):
         self.assertEqual(list(generated_tags_post), list(expected_post))
 
     def test_get_posts_excluding_tag(self):
-        tag_string = "test1 -test_two"
+        tag_string = "test1 -testtwo"
 
         generated_tags = verify_and_substitute_alias(tag_string)
 
@@ -92,7 +92,7 @@ class SearchTests(TestCase):
         self.assertEqual(list(generated_tags_post), list(expected_post))
 
     def test_get_posts_excluding_multiple_tags(self):
-        tag_string = "test1 -test_two -test3"
+        tag_string = "test1 -testtwo -test3"
 
         generated_tags = verify_and_substitute_alias(tag_string)
 
@@ -103,7 +103,7 @@ class SearchTests(TestCase):
         self.assertEqual(list(generated_tags_post), list(expected_post))
 
     def test_get_posts_with_or_operation_tags(self):
-        tag_string = "test3 ~test_two"
+        tag_string = "test3 ~testtwo"
 
         generated_tags = verify_and_substitute_alias(tag_string)
 
@@ -116,7 +116,7 @@ class SearchTests(TestCase):
         self.assertEqual(list(generated_tags_post), list(expected_post))
 
     def test_get_multiple_posts_from_multiple_tags(self):
-        tag_string = "test1 test_two"
+        tag_string = "test1 testtwo"
 
         generated_tags = verify_and_substitute_alias(tag_string)
 
