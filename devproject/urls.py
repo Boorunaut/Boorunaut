48a560979b967d766036b03e867f242523f9e345
Luk3 <luk3@hotmail.com.br>
2018-11-10 15:44:08 -0200

    Updating version of Django in devproject.

--- devproject/urls.py
+++ devproject/urls.py
@@ -1,7 +1,7 @@
 """devproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
