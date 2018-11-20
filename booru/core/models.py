321fe203d466b2362ef353abe3069c39b684299a
Luk3 <luk3@hotmail.com.br>
2018-11-20 04:29:24 -0200

    Creates new BannedHash model, makes a md5 validation against it on the form.

--- booru/core/models.py
+++ booru/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BannedHash(models.Model):
+    content = models.CharField(max_length=100, blank=False)
+    
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name_plural = 'Banned hashes (MD5)'
