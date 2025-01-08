1552137626d7026d7be0db2d8307c7ed276cd3fa
LucasCTN <lucascampostn@gmail.com>
2025-01-08 19:44:43 -0300

    Adds environment variables on settings.py

--- devproject/settings.py
+++ devproject/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@jh*9d-=6w3b7g6c!cxy(mjfpt+=$gj@4*tualb03$+dwset#z'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='@jh*9d-=6w3b7g6c!cxy(mjfpt+=$gj@4*tualb03$+dwset#z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DEBUG_MODE', default=True)
 
 ALLOWED_HOSTS = []
+
+host = os.environ.get('SERVER_HOST')
+if host:
+    ALLOWED_HOSTS.append(host)
 
 AUTH_USER_MODEL = 'account.Account'
 
